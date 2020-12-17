3483b2edc5187fff47ea1ca3d8b1f19311626b4d
Thomas Eizinger <thomas@eizinger.io>
2020-12-17 11:08:10 +1100

    Guard header files from being included several times
    For some reason, this is required when targeting wasm but is fine
    on x86 (maybe a difference between clang and gcc?).
    
    In any case, it doesn't hurt to #ifdef guard these headers.

--- secp256k1-zkp-sys/wasm-sysroot/string.h
+++ secp256k1-zkp-sys/wasm-sysroot/string.h
@@ -1,4 +1,9 @@
+#ifndef WASM_SYSROOT_STRING_H
+#define WASM_SYSROOT_STRING_H
+
 #include <stddef.h>
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
+
+#endif
