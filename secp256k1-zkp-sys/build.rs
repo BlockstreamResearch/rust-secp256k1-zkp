163186ae40317fcc93d0deb1940338d31f583ee3
Sanket Kanjalkar <sanketk@squareup.com>
2024-01-07 08:31:55 -0800

    secp256k1-zkp-sys: Add Rust FFI for Musig2 module

--- secp256k1-zkp-sys/build.rs
+++ secp256k1-zkp-sys/build.rs
@@ -44,6 +44,9 @@ fn main() {
         .define("ENABLE_MODULE_RANGEPROOF", Some("1"))
         .define("ENABLE_MODULE_ECDSA_ADAPTOR", Some("1"))
         .define("ENABLE_MODULE_WHITELIST", Some("1"))
+        .define("ENABLE_MODULE_EXTRAKEYS", Some("1"))
+        .define("ENABLE_MODULE_MUSIG", Some("1"))
+        .define("ENABLE_MODULE_SCHNORRSIG", Some("1"))
         .define("ECMULT_GEN_PREC_BITS", Some("4"))
         // TODO these three should be changed to use libgmp, at least until secp PR 290 is merged
         .define("USE_NUM_NONE", Some("1"))
